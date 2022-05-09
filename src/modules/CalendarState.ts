85111446dabbe1a275e06f621a9f9f310d7c0504
Huang Xingqiao <a92304a92304@gmail.com>
2022-05-09 20:22:13 +0800

    feat: create class `CalendarState`

---
+++ src/modules/CalendarState.ts
@@ -0,0 +1,73 @@
+import dayjs, { ConfigType, Dayjs } from 'dayjs'
+import objectSupport from 'dayjs/plugin/objectSupport'
+import localeData from 'dayjs/plugin/localeData'
+
+dayjs.extend(objectSupport)
+dayjs.extend(localeData)
+
+/** Immutable 的月曆狀態 */
+export default class CalendarState {
+  date: Dayjs
+
+  constructor (date?: ConfigType) {
+    this.date = dayjs(date).startOf('month')
+  }
+
+  /** 設定新的年份並回傳新的 CalendarState 實體 */
+  setYear (year: number) {
+    return new CalendarState(this.date.set({ year }))
+  }
+
+  /** 設定新的月份並回傳新的 CalendarState 實體 */
+  setMonth (month: number) {
+    return new CalendarState(this.date.set({ month }))
+  }
+
+  /** 檢查傳入的參數是否與目前月曆同年份 */
+  checkIsSameYear (date?: ConfigType) {
+    return this.date.isSame(date, 'year')
+  }
+
+  /** 檢查傳入的參數是否與目前月曆同月份 */
+  checkIsSameMonth (date?: ConfigType) {
+    return this.date.isSame(date, 'month')
+  }
+
+  get matrix () {
+    const matrix: Dayjs[][] = Array.from({ length: 6 })
+      .map(() => Array.from({ length: 7 }))
+
+    /** 本月第一天的星期 */
+    const firstDayOfWeek = this.date.day()
+
+    for (let week = 0; week < 6; week++) {
+      for (let day = 0; day < 7; day++) {
+        matrix[week][day] = this.date.add(
+          week * 7 + day - firstDayOfWeek,
+          'd'
+        )
+      }
+    }
+    return matrix
+  }
+
+  get years () {
+    const startOfYears = Math.floor(this.date.get('year') / 10) * 10
+    return Array.from({ length: 10 })
+      .map((_, index) => index + startOfYears)
+  }
+
+  get title () {
+    return this.date.format('MMM YYYY')
+  }
+
+  get nextMonth () {
+    return this.date.add(1, 'month')
+  }
+
+  get previousMonth () {
+    return this.date.add(-1, 'month')
+  }
+
+  static months = dayjs.monthsShort()
+}
