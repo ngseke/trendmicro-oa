fc44e6cd093e85e8bc2414d1a75bca2d0ff87f63
Huang Xingqiao <a92304a92304@gmail.com>
2022-05-09 22:34:03 +0800

    feat: create month view

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import logo from './logo.svg'
+import { useState } from 'react'
+import Calendar from './components/Calendar'
 
-function App () {
+export default function App () {
+  const [date, setDate] = useState(new Date())
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar date={date} onSelect={setDate} />
     </div>
   )
 }
-
-export default App
---
+++ src/components/Calendar.tsx
@@ -0,0 +1,77 @@
+import dayjs from 'dayjs'
+import { useMemo, useState } from 'react'
+import CalendarState from '../modules/CalendarState'
+import CalendarCell from './CalendarCell'
+import CalendarNavbar from './CalendarNavbar'
+
+interface CalendarProps {
+  date?: Date | null,
+  onSelect? (date: Date): void,
+}
+
+type View = 'date' | 'month' | 'year'
+
+const today = dayjs()
+
+export default function Calendar ({ date, onSelect }: CalendarProps) {
+  const [view, setView] = useState<View>('date')
+  const [calendar, setCalendar] = useState(new CalendarState())
+
+  const title = useMemo(() => {
+    return {
+      date: calendar.dateViewTitle,
+      month: calendar.monthViewTitle,
+      year: calendar.yearViewTitle,
+    }[view]
+  }, [calendar, view])
+
+  const handleClickPrevious = () => {
+    if (view === 'date') {
+      setCalendar(calendar.previousMonth)
+    }
+  }
+
+  const handleClickNext = () => {
+    if (view === 'date') {
+      setCalendar(calendar.nextMonth)
+    }
+  }
+
+  return (
+    <div className="p-1 border-[1px] border-gray-500 w-[250px]">
+      <CalendarNavbar
+        onClickPrevious={handleClickPrevious}
+        onClickNext={handleClickNext}
+      >
+        {title}
+      </CalendarNavbar>
+
+      <div className="flex flex-col items-stretch">
+        {
+          view === 'date' &&
+            calendar.matrix.map((row, key) => (
+              <div className="flex space-y-1" key={key}>
+                {
+                  row.map((cell, key) => (
+                    <div
+                      className="flex flex-1 justify-center items-center"
+                      key={key}
+                    >
+                      <CalendarCell
+                        onClick={() => onSelect?.(cell.toDate())}
+                        disabled={!calendar.checkIsSameMonth(cell)}
+                        active={cell.isSame(date, 'date')}
+                        today={cell.isSame(today, 'date')}
+                      >
+                        {cell.get('date')}
+                      </CalendarCell>
+                    </div>
+                  ))
+                }
+              </div>
+            ))
+        }
+      </div>
+    </div>
+  )
+}
